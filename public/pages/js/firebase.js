a0bc55709576470973a88bc04db4cd418372ff8a
Christopher Mills <christopher.mills-cic-uk@ibm.com>
2024-04-17 15:13:39 +0100

    logging firebase and auth

--- public/pages/js/firebase.js
+++ public/pages/js/firebase.js
@@ -1,17 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgkCUR0Y-7jXurdxrVDYNxQVRQidiNJBw",
-  authDomain: "code-the-future-hybrid.firebaseapp.com",
-  projectId: "code-the-future-hybrid",
-  storageBucket: "code-the-future-hybrid.appspot.com",
-  messagingSenderId: "872086354850",
-  appId: "1:872086354850:web:092e805b04287ae7fcd0fb",
-  measurementId: "G-JSR5R0X09W",
-  databaseURL: "https://code-the-future-hybrid-default-rtdb.europe-west1.firebasedatabase.app/"
+  apiKey: "AIzaSyA1M-spdbLBikmMNzAol0m3uE0_WZYAXWU",
+  authDomain: "pokemon-firebase-820fd.firebaseapp.com",
+  projectId: "pokemon-firebase-820fd",
+  storageBucket: "pokemon-firebase-820fd.appspot.com",
+  messagingSenderId: "866190768035",
+  appId: "1:866190768035:web:f63814c66decac85d28c16",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 export const fbAuth = firebase.auth();
-export const fbDB = firebase.database()
-export const fbStorage = firebase.storage()
+export const fbDB = firebase.database();
+export const fbStorage = firebase.storage();
